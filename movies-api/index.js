f014e5a33b5999e20bd2c608b2ec7d81fa77d14f
Lucas Heim <lucasedfc@gmail.com>
2020-12-07 20:47:46 -0300

    Express Server

---
+++ movies-api/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const { config } = require('./config/index');
+
+
+app.get('/', function(req, res) {
+    res.send("Hello World")
+});
+
+app.get('/json', function(req, res) {
+    res.json({hello: 'world'})
+});
+
+app.listen(config.port, function() {
+    console.log(`Listening http://localhost:${config.port}`);
+})
