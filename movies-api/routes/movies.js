13fb5fdfb8ae6fee0c086ef05ae42a74021b2d89
Lucas Heim <lucasedfc@gmail.com>
2020-12-12 15:04:52 -0300

    Movies API CRUD

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -19,6 +19,64 @@ function moviesApi(app) {
             next(error);
         }
     });
+    
+    router.get('/:movieId', async function(req, res, next) {
+
+        try {
+            const movies = await Promise.resolve(moviesMock[0]);
+
+            res.status(200).json({
+                data: movies,
+                message: 'Movie Retrieved'
+            })
+        } catch (error) {
+            next(error);
+        }
+    });
+
+
+    router.post('/', async function(req, res, next) {
+
+
+        try {
+            const createdMovieId = await Promise.resolve(moviesMock[0].id);
+            res.status(201).json({
+                data: createdMovieId,
+                message: 'Movie Created'
+            })
+        } catch (error) {
+            next(error);
+        }
+    });
+
+
+    router.put('/:movieId', async function(req, res, next) {
+
+        try {
+            const updatedMovieId = await Promise.resolve(moviesMock[0].id);
+
+            res.status(200).json({
+                data: updatedMovieId,
+                message: 'Movie Updated'
+            })
+        } catch (error) {
+            next(error);
+        }
+    });
+
+    router.delete('/:movieId', async function(req, res, next) {
+
+        try {
+            const deletedMovieId = await Promise.resolve(moviesMock[0].id);
+
+            res.status(200).json({
+                data: deletedMovieId,
+                message: 'Movie Deleted'
+            })
+        } catch (error) {
+            next(error);
+        }
+    });
 }
 
 module.exports = moviesApi;
